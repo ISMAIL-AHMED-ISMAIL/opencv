754c557cb3fcb095d4292715498f264224d72b71
ISMAIL-AHMED-ISMAIL <ismailahmedismail1111@gmail.com>
2026-02-13 17:49:13 +0200

    Change image caption to 'ERROR'
    Update image display caption to 'ERROR'.

--- app.py
+++ app.py
@@ -2,12 +2,8 @@ import streamlit as st
 import cv2
 import numpy as np
 
-st.title("تحليل صورة العربية🚗")
 file = st.file_uploader("ارفع صورة العربية من موبايلك")
 
 if file:
     img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), 1)
-    st.image(img, channels="BGR", caption="هذه هي الصورة التي قرأها opencv")
-    st.subheader("مصفوفه بكسلات الصوره (الارقام):")
-    st.text(str(img))
-    st.write(f"أبعاد هذه المصفوفة هي: {img.shape}")
+    st.image(img, channels="BGR", caption="ERROR")
