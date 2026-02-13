2073df1ccd8292597c831fa14dfcd2ad5d9b3204
ISMAIL-AHMED-ISMAIL <ismailahmedismail1111@gmail.com>
2026-02-13 20:04:33 +0200

    Correct pixel count and dimensions output
    Fix pixel count and dimensions display in image uploader.

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ file = st.file_uploader("ارفع صورة العربية من موبايلك")
 
 if file:
     img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), 1)
+    pixels = img.size
+    dimensions = img.shap
     st.image(img, channels="BGR", caption="ERROR")
+    st.write(f"Number of pixels: "{pixels}")
+    st.write(f"Dimensions: "{dimensions}")
