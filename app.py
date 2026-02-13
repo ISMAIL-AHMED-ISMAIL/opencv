8ea4b44c501108e97ba62edf475ab72b1e92b5ed
ISMAIL-AHMED-ISMAIL <ismailahmedismail1111@gmail.com>
2026-02-13 20:10:16 +0200

    Fix typo in image dimensions variable

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ file = st.file_uploader("ارفع صورة العربية من موبايلك")
 if file:
     img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), 1)
     pixels = img.size
-    dimensions = img.shap
+    dimensions = img.shape
     st.image(img, channels="BGR", caption="ERROR")
     st.write(f"Number of pixels: {pixels}")
     st.write(f"Dimensions: {dimensions}")
